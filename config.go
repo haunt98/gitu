9508c2d0e5f92bce9cebb8cfd40aabe130661921
hau <ngtranhau@gmail.com>
2021-01-06 16:05:39 +0700

    feat(config): add load config from file

---
+++ config.go
@@ -0,0 +1,80 @@
+// Config stores at $XDG_CONFIG_HOME/gitu/config.json
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/haunt98/xdg"
+)
+
+const (
+	configFile = "config.json"
+)
+
+type Config struct {
+	Users map[string]User `json:"users"`
+}
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+// Load config from file, return empty if file not found
+func Load() (Config, error) {
+	path := getPath()
+	f, err := os.Open(path)
+	if err != nil {
+		// https://github.com/golang/go/blob/3e1e13ce6d1271f49f3d8ee359689145a6995bad/src/os/error.go#L90-L91
+		if errors.Is(err, os.ErrNotExist) {
+			return Config{
+				Users: make(map[string]User),
+			}, nil
+		}
+
+		return Config{}, fmt.Errorf("failed to open %s: %w", path, err)
+	}
+	defer f.Close()
+
+	bytes, err := ioutil.ReadAll(f)
+	if err != nil {
+		return Config{}, fmt.Errorf("failed to read %s: %w", path, err)
+	}
+
+	var result Config
+	if err := json.Unmarshal(bytes, &result); err != nil {
+		return Config{}, fmt.Errorf("failed to unmarshal: %w,", err)
+	}
+
+	if result.Users == nil {
+		result.Users = make(map[string]User)
+	}
+
+	return result, nil
+}
+
+func Save(c *Config) {
+}
+
+func (c *Config) CheckExist(nickname string) bool {
+	_, ok := c.Users[nickname]
+	return ok
+}
+
+func (c *Config) Update(nickname string, user User) {
+	c.Users[nickname] = user
+}
+
+func (c *Config) Delete(nickname string) {
+	delete(c.Users, nickname)
+}
+
+func getPath() string {
+	return filepath.Join(xdg.GetConfigHome(), name, configFile)
+}
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
@@ -13,14 +15,38 @@ const (
 	name = "gitu"
 
 	debugFlag = "debug"
+	nameFlag  = "name"
+	emailFlag = "email"
+
+	addCommand = "add"
 )
 
 func main() {
-	a := &action{}
+	a := &action{
+		flags: make(map[string]string),
+	}
 
 	app := &cli.App{
 		Name:  name,
 		Usage: "switch git user",
+		Commands: []*cli.Command{
+			{
+				Name:    addCommand,
+				Aliases: []string{"a"},
+				Usage:   "add git user",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:  nameFlag,
+						Usage: "gitconfig user.name",
+					},
+					&cli.StringFlag{
+						Name:  emailFlag,
+						Usage: "gitconfig user.email",
+					},
+				},
+				Action: a.RunAdd,
+			},
+		},
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:    debugFlag,
@@ -41,12 +67,27 @@ func main() {
 
 type action struct {
 	debug bool
+	flags map[string]string
 }
 
+// Show help by default
 func (a *action) Run(c *cli.Context) error {
-	// Show help if there is nothing
-	if c.NArg() == 0 && c.NumFlags() == 0 {
-		return cli.ShowAppHelp(c)
+	return cli.ShowAppHelp(c)
+}
+
+func (a *action) RunAdd(c *cli.Context) error {
+	a.getFlags(c)
+
+	if a.flags[nameFlag] == "" {
+		fmt.Println("What is your name?")
+		a.flags[nameFlag] = readStdin()
+		fmt.Printf("Hello %s\n", a.flags[nameFlag])
+	}
+
+	if a.flags[emailFlag] == "" {
+		fmt.Println("What is your email?")
+		a.flags[emailFlag] = readStdin()
+		fmt.Printf("Copy that %s\n", a.flags[emailFlag])
 	}
 
 	return nil
@@ -54,10 +95,28 @@ func (a *action) Run(c *cli.Context) error {
 
 func (a *action) getFlags(c *cli.Context) {
 	a.debug = c.Bool(debugFlag)
+	a.flags[nameFlag] = c.String(nameFlag)
+	a.flags[emailFlag] = c.String(emailFlag)
 }
 
 func (a *action) logDebug(format string, v ...interface{}) {
 	if a.debug {
 		log.Printf(format, v...)
 	}
+}
+
+func readStdin() string {
+	bs := bufio.NewScanner(os.Stdin)
+	for bs.Scan() {
+		line := bs.Text()
+		line = strings.TrimSpace(line)
+		if line == "" {
+			continue
+		}
+
+		return line
+		break
+	}
+
+	return ""
 }
