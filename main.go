a2d83e704d411aff40d253863e88969fa1faa511
hau <ngtranhau@gmail.com>
2021-01-06 11:45:06 +0700

    feat(cli): skeleton cli app

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli"
+)
+
+const (
+	name = "gitu"
+)
+
+func main() {
+	a := &action{}
+
+	app := &cli.App{
+		Name:   name,
+		Usage:  "switch git user",
+		Action: a.Run,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		// Highlight error
+		fmtErr := color.New(color.FgRed)
+		fmtErr.Printf("[%s error]: ", name)
+		fmt.Printf("%s\n", err.Error())
+	}
+}
+
+type action struct {
+	debug bool
+}
+
+func (a *action) Run(c *cli.Context) error {
+	// Show help if there is nothing
+	if c.NArg() == 0 && c.NumFlags() == 0 {
+		return cli.ShowAppHelp(c)
+	}
+
+	return nil
+}
+
+func (a *action) logDebug(format string, v ...interface{}) {
+	if a.debug {
+		log.Printf(format, v...)
+	}
+}
